a02df136e5bb25f0e885e7627ebb065d8f71db48
yestolife <zgjxncytl@gmail.com>
2024-04-04 21:19:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import gradio as gr
 from modelscope import AutoModelForCausalLM, AutoTokenizer
-
+import os
+from swift.llm import get_model_tokenizer, ModelType
+from swift.tuners import Swift
 
 def load_model():
-    model = AutoModelForCausalLM.from_pretrained(
-        "qwen/qwen-1_8b-chat",
-        trust_remote_code=True,
-        device_map="auto"
-    )
-    tokenizer = AutoTokenizer.from_pretrained("qwen/qwen-1_8b-chat",trust_remote_code=True)
+    ckpt_dir = 'checkpoint-280'
+    model_type = ModelType.qwen_7b_chat
+    model, tokenizer = get_model_tokenizer(model_type, model_kwargs={'device_map': 'auto'})
+    model = Swift.from_pretrained(model, ckpt_dir, inference_mode=True)
+    # model = AutoModelForCausalLM.from_pretrained(
+    #     "qwen/qwen-1_8b-chat",
+    #     trust_remote_code=True,
+    #     device_map="auto"
+    # )
+    # tokenizer = AutoTokenizer.from_pretrained("qwen/qwen-1_8b-chat",trust_remote_code=True)
     return model, tokenizer
 
 
@@ -36,6 +42,8 @@ def news_writer(prompt):
     
 model, tokenizer = load_model()
 device = "cuda"
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
 webui = gr.Interface(
     news_writer, 
     inputs=[gr.Textbox(label="输入主要内容", lines=5)],
